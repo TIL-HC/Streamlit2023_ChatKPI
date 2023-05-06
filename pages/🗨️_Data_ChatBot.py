8db1f8a5f3120ab23224fdcaf905147f2f23a3cc
TIL-HC <118545100+TIL-HC@users.noreply.github.com>
2023-05-06 08:42:29 +0100

    Add files via upload

--- pages/🗨️_Data_ChatBot.py
+++ pages/🗨️_Data_ChatBot.py
@@ -24,61 +24,7 @@ st.markdown("Type a question in the box below and hit submit to ask the AI bot a
 
 st.divider()
 
-# Function to connect to Snowflake
-def connect_to_sf(acct,usr,pwd,rl,wh,db):
-    ctx = connect(user=usr,account=acct,password=pwd,role=rl,warehouse=wh,database=db)
-    cs = ctx.cursor()
-    st.session_state['snow_conn'] = cs
-    st.session_state['ready'] = True
-    return cs
-
-# Function to get Snowflake data
-def get_sf_data():
-    query = f'SELECT * FROM STREAMLIT_COMP_DB.STREAMLIT_COMP_SCHMA.PART_ORDERS LIMIT 500' 
-    results = st.session_state['snow_conn'].execute(query)
-    results = st.session_state['snow_conn'].fetch_pandas_all()
-    return results
-
-# Load data from Snowflake
-@st.cache_data
-def load_sf_data_source():
-    connect_to_sf(st.secrets['sf_creds']["account"],st.secrets['sf_creds']["username"],st.secrets['sf_creds']["password"], \
-                st.secrets['sf_creds']["role"],st.secrets['sf_creds']["warehouse"],st.secrets['sf_creds']["database"])
-    if 'ready' not in st.session_state:
-        st.session_state['ready'] = False
-
-    if st.session_state['ready'] == True:
-        df = get_sf_data()
-        return df
-
-# Load data from uploaded file
-@st.cache_data
-def load_file_data_source(uploaded_file):
-    if uploaded_file is not None:
-        df = pd.read_csv(uploaded_file)
-        st.session_state['ready'] = True
-        return df
-
-
-# Show sidebar UI
-with st.sidebar:
-    data_source_type = st.selectbox('Data Source Type', ['❄️ Snowflake', '📁 File'])
-
-    if data_source_type == '❄️ Snowflake':
-        st.session_state.ds_type_sf = True
-        
-        st.session_state.df = load_sf_data_source()
-
-    else:
-        st.session_state.ds_type_sf = False
-        st.session_state.ready = False
-
-
-    # Show file upload UI
-    if not st.session_state.ds_type_sf:
-        uploaded_file = st.file_uploader('Upload a CSV File', type='csv')
-        st.session_state.df = load_file_data_source(uploaded_file)
-
+st.dataframe(st.session_state.df)
 
 open_ai_key = st.secrets['api_key']["open_ai_key"]
 os.environ['OPENAI_API_KEY'] = open_ai_key
@@ -89,7 +35,7 @@ llm = OpenAI(temperature=0,
             verbose=False) 
 
 
-index = GPTPandasIndex(df=st.session_state['df'])
+index = GPTPandasIndex(df=st.session_state.df)
 
 query_engine = index.as_query_engine(
     verbose=True
